bec111364643048da8ff23a5378f38bc8640db45
Christian Tegethoff <christian.tegethoff@kuehne-nagel.com>
2022-08-26 12:22:02 +0200

    make reinstall doom independet from current dir

--- reinstall_doom.sh
+++ reinstall_doom.sh
@@ -5,6 +5,6 @@ CURRENTDATE=`date +"%Y-%m-%d"`
 # echo Date is: ${CURRENTDATE}
 # echo Date is: `date +"%Y-%m-%d"`
 
-mv .emacs.d .emacs.d.${CURRENTDATE}
+mv /home/chris/.emacs.d /home/chris/.emacs.d.${CURRENTDATE}
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
